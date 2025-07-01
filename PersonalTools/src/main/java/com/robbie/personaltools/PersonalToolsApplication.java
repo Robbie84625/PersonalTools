ae7f0322b507e9e802df1e023b1a0b38049d1158
robbie.liu <robbie.liu@nextbank.com.tw>
2025-07-01 17:08:32 +0800

    Initial - Spring Boot project setup

---
+++ PersonalTools/src/main/java/com/robbie/personaltools/PersonalToolsApplication.java
@@ -0,0 +1,13 @@
+package com.robbie.personaltools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalToolsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PersonalToolsApplication.class, args);
+  }
+
+}
---
+++ PersonalTools/src/test/java/com/robbie/personaltools/PersonalToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robbie.personaltools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalToolsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
